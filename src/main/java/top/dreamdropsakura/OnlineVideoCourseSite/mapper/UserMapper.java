d43f12962cb6ffecdc0ad0f4e3dca0b0d3384ead
TheFairyTale <986920241@qq.com>
2021-10-11 23:29:26 +0800

    create UserMapper interface and extend BaseMapper class, it can use CRUD action. (JPA, Java Persistence API)

---
+++ src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package top.dreamdropsakura.OnlineVideoCourseSite.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import top.dreamdropsakura.OnlineVideoCourseSite.entity.User;
+
+// 接口"UserMapper" 继承 BaseMapper 并写上实体类，即可调用增删改查功能
+public interface UserMapper extends BaseMapper<User> {
+}
