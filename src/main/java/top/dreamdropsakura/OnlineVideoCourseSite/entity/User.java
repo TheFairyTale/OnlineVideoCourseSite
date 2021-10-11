677abc6f2edc3480bad721299fa9e7cfc9cd18d1
TheFairyTale <986920241@qq.com>
2021-10-11 23:30:35 +0800

    add commit.

--- src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/entity/User.java
+++ src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/entity/User.java
@@ -2,6 +2,7 @@ package top.dreamdropsakura.OnlineVideoCourseSite.entity;
 
 import lombok.Data;
 
+// 加上@Data 后就不用为属性写get / set 和无参构造方法的代码了，lombok 会自动生成
 @Data
 public class User {
     private Long id;
