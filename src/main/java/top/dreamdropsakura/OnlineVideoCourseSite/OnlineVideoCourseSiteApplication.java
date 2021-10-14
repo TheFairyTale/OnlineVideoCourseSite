316f35ced358524f735e15c702c368daf8542fc8
TheFairyTale <986920241@qq.com>
2021-10-14 16:17:03 +0800

    @MapperScan(包名) 可用于找到mapper 文件夹中的接口

--- src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/OnlineVideoCourseSiteApplication.java
+++ src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/OnlineVideoCourseSiteApplication.java
@@ -1,9 +1,12 @@
 package top.dreamdropsakura.OnlineVideoCourseSite;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 可用于找到mapper 中的接口（？（括号内放mapper 的包名）
+@MapperScan("top.dreamdropsakura.OnlineVideoCourseSite.mapper")
 public class OnlineVideoCourseSiteApplication {
 
 	public static void main(String[] args) {
