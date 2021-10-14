6d86d59f169b707bf82b269cc1e7a54aacbb3b39
TheFairyTale <986920241@qq.com>
2021-10-14 16:21:01 +0800

    使用单元测试来将mp 做最终实现

--- src/test/java/top/dreamdropsakura/OnlineVideoCourseSite/OnlineVideoCourseSiteApplicationTests.java
+++ src/test/java/top/dreamdropsakura/OnlineVideoCourseSite/OnlineVideoCourseSiteApplicationTests.java
@@ -6,6 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OnlineVideoCourseSiteApplicationTests {
 
+	// 单元测试
+	// 利用单元测试，将mapper 做最终实现
+
+	// 测试第一个功能
+	// 查询User 表中所有数据
+
+
 	@Test
 	void contextLoads() {
 	}
