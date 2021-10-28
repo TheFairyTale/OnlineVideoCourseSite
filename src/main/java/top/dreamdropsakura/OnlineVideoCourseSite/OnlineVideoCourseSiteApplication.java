6a0c94b5d128226d88440d1503b2a221c23b21be
TheFairyTale <986920241@qq.com>
2021-10-28 20:00:37 +0800

    新建配置类，用于存放所有配置

--- src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/OnlineVideoCourseSiteApplication.java
+++ src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/OnlineVideoCourseSiteApplication.java
@@ -1,16 +1,13 @@
 package top.dreamdropsakura.OnlineVideoCourseSite;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// 可用于找到mapper 中的接口（？（括号内放mapper 的包名）
-@MapperScan("top.dreamdropsakura.OnlineVideoCourseSite.mapper")
 public class OnlineVideoCourseSiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineVideoCourseSiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineVideoCourseSiteApplication.class, args);
+    }
 
 }
---
+++ src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/config/MpConfig.java
@@ -0,0 +1,22 @@
+package top.dreamdropsakura.OnlineVideoCourseSite.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// 可用于找到mapper 中的接口（？（括号内放mapper 的包名）
+@MapperScan("top.dreamdropsakura.OnlineVideoCourseSite.mapper")
+public class MpConfig {
+    /*
+     * 乐观锁插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+}
