888de1fdc4b82251a9c94f04822a8711bce0ad2d
TheFairyTale <986920241@qq.com>
2021-11-12 20:01:00 +0800

    创建项目：先创建父工程，父工程是pom类型，用于管理依赖版本，或放一些公共依赖。然后在父工程中创建子模块（即子工程），一个工程中有多个不同的子模块。有时为了项目更加清晰，子模块中可能还有单或多层的子模块嵌套其中。而这也是项目的扩建方式。在当前这个项目(OnlineVideoCourseSite)当中，父工程（最顶级的工程）为spring boot 工程。下一级的工程为maven 工程。下下级的为maven 工程。
    该项目(OnlineVideoCourseSite)的模块说明：
    guli-parent：在线教学根目录（父工程），管理四个子模块：
        1.canal-client：canal数据库表同步模块（统计同步数据）
        2.common：公共模块父节点(3个子模块)
            1.common-util：工具类模块，所有模块都可以依赖于它
            2.service-base：service服务的base包，包含service服务的公共配置类，所有service模块依赖于它
            3.spring-security：认证与授权模块，需要认证授权的service服务依赖于它
        3.infrastructure：基础服务模块父节点（1个子模块）
            1.api-gateway：api网关服务
        4.service：api接口服务父节点
    1.service-acl：用户权限管理api接口服务（用户管理、角色管理和权限管理等）
    2.service-cms：cms api接口服务
    3.service-edu：教学相关api接口服务
    4.service-msm：短信api接口服务
    5.service-order：订单相关api接口服务
    6.service-oss：阿里云oss api接口服务
    7.service-statistics：统计报表api接口服务
    8.service-ucenter：会员api接口服务
    9.service-vod：视频点播api接口服务

--- src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/OnlineVideoCourseSiteApplication.java
+++ src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/OnlineVideoCourseSiteApplication.java
@@ -9,5 +9,4 @@ public class OnlineVideoCourseSiteApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnlineVideoCourseSiteApplication.class, args);
     }
-
 }
