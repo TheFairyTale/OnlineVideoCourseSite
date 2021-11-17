c4844991844e04b29c073ac678cf108142d6278e
TheFairyTale <986920241@qq.com>
2021-11-17 13:42:33 +0800

    父工程的代码写在子模块中。故不需要src 文件夹

--- src/main/java/top/dreamdropsakura/OnlineVideoCourseSite/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package top.dreamdropsakura.OnlineVideoCourseSite.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import top.dreamdropsakura.OnlineVideoCourseSite.entity.Users;
-
-// 接口"UserMapper" 继承 BaseMapper 并写上实体类，即可调用增删改查功能
-public interface UserMapper extends BaseMapper<Users> {
-
-}
