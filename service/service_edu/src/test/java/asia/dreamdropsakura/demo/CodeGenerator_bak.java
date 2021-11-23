47dc541d66b20a71102f1eb40735254681f57be5
TheFairyTale <986920241@qq.com>
2021-11-23 18:15:37 +0800

    update file

--- service/service_edu/src/test/java/asia/dreamdropsakura/demo/CodeGenerator_bak.java
+++ service/service_edu/src/test/java/asia/dreamdropsakura/demo/CodeGenerator_bak.java
@@ -167,6 +167,7 @@ public class CodeGenerator_bak {
 
         //
         mpg.setStrategy(strategy);
+        // 设置velocityTemplateEngine
         mpg.setTemplateEngine(new FreemarkerTemplateEngine());
         // 执行
         mpg.execute();
