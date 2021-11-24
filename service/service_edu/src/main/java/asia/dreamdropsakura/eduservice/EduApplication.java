2ff6d23d23679b1bf46324a0a0b375e21edcc69d
TheFairyTale <986920241@qq.com>
2021-11-24 11:33:13 +0800

    创建该模块的启动类

---
+++ service/service_edu/src/main/java/asia/dreamdropsakura/eduservice/EduApplication.java
@@ -0,0 +1,9 @@
+package asia.dreamdropsakura.eduservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class EduApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class, args);
+    }
+}
