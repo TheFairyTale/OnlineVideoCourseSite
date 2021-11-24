2efcde821045347d4ad7de744b729639fd2dc2b3
TheFairyTale <986920241@qq.com>
2021-11-24 13:31:53 +0800

    修复SpringBoot 启动时必须的注解。该注解用于快捷配置启动类，其中包含其他的注解

--- service/service_edu/src/main/java/asia/dreamdropsakura/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/asia/dreamdropsakura/eduservice/EduApplication.java
@@ -1,7 +1,9 @@
 package asia.dreamdropsakura.eduservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
