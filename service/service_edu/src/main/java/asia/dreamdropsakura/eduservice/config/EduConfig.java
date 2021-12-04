ef9b94d55ca906be2bf2cdff3fee07ec0374a97d
TheFairyTale <986920241@qq.com>
2021-12-04 21:52:16 +0800

    添加分类插件

--- service/service_edu/src/main/java/asia/dreamdropsakura/eduservice/config/EduConfig.java
+++ service/service_edu/src/main/java/asia/dreamdropsakura/eduservice/config/EduConfig.java
@@ -1,6 +1,10 @@
 package asia.dreamdropsakura.eduservice.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -8,5 +12,11 @@ import org.springframework.context.annotation.Configuration;
 // @MapperScan("mapper interface 类的包的地址。例如asia.dreamdropsakura.eduservice.mapper。用的类是eduservice 包下的mapper/EduTeacherMapper.java")
 @MapperScan("asia.dreamdropsakura.eduservice.mapper")
 public class EduConfig {
-
+    // mybatis plus 分页插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
 }
