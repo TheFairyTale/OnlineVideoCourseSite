239ec3acbd962e8a3533523ac809bfc50be4cfcc
TheFairyTale <986920241@qq.com>
2021-12-09 22:56:48 +0800

    数据库表字段值自更新

---
+++ common/service_base/src/main/java/asia/dreamdropsakura/servicebase/handler/ServiceBaseMetaObjectHandler.java
@@ -0,0 +1,30 @@
+package asia.dreamdropsakura.servicebase.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * <p>
+ * 数据表字段自更新 公共组件
+ * </p>
+ *
+ * @author TheFairyTale
+ * @since 2021-12-09
+ */
+@Component
+public class ServiceBaseMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+}
