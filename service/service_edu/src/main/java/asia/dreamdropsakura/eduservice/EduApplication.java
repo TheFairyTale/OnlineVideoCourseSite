1dccefcc0e4b94df455a2ae624f02942ce87e6bb
TheFairyTale <986920241@qq.com>
2021-12-09 22:59:06 +0800

    update commit.

--- service/service_edu/src/main/java/asia/dreamdropsakura/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/asia/dreamdropsakura/eduservice/EduApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * 项目组件启动类
+ *
+ * @author TheFairyTale
+ */
 @SpringBootApplication
 // @ComponentScan(basePackages = {}) 扫描指定的其他包中的类并加载(即定义要扫描的路径，从中找出标识了需要装配的类并自动装配到
 // spring 的bean 容器中)。{} 为一个数组
