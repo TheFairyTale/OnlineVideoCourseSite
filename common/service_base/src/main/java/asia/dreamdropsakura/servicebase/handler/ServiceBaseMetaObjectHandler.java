3738451e8384ce38fb8ddf9186ae7e9c6c291f1b
TheFairyTale <986920241@qq.com>
2021-12-16 19:42:09 +0800

    终于解决了前端传入的时间参数的值不带 时分秒 的问题，在ServiceBaseMetaObjectHandler 中使用setFieldValByName() 并使第一个参为实体类属性名，第二参为Timestamp() 的实例化对象，其中传入System.currentTimeMillis() 来生成带时分秒的时间。即可。

--- common/service_base/src/main/java/asia/dreamdropsakura/servicebase/handler/ServiceBaseMetaObjectHandler.java
+++ common/service_base/src/main/java/asia/dreamdropsakura/servicebase/handler/ServiceBaseMetaObjectHandler.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
 /**
@@ -15,7 +17,7 @@ import java.text.SimpleDateFormat;
  * @author TheFairyTale
  * @since 2021-12-09
  */
-//@Component
+@Component
 public class ServiceBaseMetaObjectHandler implements MetaObjectHandler {
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private final Logger logger = LogManager.getLogger("name");
@@ -24,15 +26,15 @@ public class ServiceBaseMetaObjectHandler implements MetaObjectHandler {
     public void insertFill(MetaObject metaObject) {
         // fieldName 数据表中的字段名称，不是实体类中的属性名称
         logger.info("insert start ...");
-        //this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtCreate", new Timestamp(System.currentTimeMillis()), metaObject);
         //this.strictInsertFill(metaObject, "gmt_create", Date.class, new Date());
-        //this.setFieldValByName("gmtModified", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Timestamp(System.currentTimeMillis()), metaObject);
         //this.strictInsertFill(metaObject, "gmtModified", Date.class, new Date());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         logger.info("insert start ...");
-        //this.setFieldValByName("gmtModified", dateFormat.format(DateFormat.getDateTimeInstance()), metaObject);
+        this.setFieldValByName("gmtModified", new Timestamp(System.currentTimeMillis()), metaObject);
     }
 }
